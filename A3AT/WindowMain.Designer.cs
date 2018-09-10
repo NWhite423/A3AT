aa9034cd58113deeaf32a4bc5e3e209b5f568ac4
nwhitemisc <nwhitemisc@gmail.com>
2018-09-10 18:01:49 -0400

    Started work on front end

--- A3AT/WindowMain.Designer.cs
+++ A3AT/WindowMain.Designer.cs
@@ -32,13 +32,6 @@
             this.gbServers = new System.Windows.Forms.GroupBox();
             this.cmdExit = new System.Windows.Forms.Button();
             this.cmdConfirm = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.gbServers.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -53,12 +46,6 @@
             // 
             // gbServers
             // 
-            this.gbServers.Controls.Add(this.button3);
-            this.gbServers.Controls.Add(this.button2);
-            this.gbServers.Controls.Add(this.button1);
-            this.gbServers.Controls.Add(this.label4);
-            this.gbServers.Controls.Add(this.label3);
-            this.gbServers.Controls.Add(this.label2);
             this.gbServers.Location = new System.Drawing.Point(12, 78);
             this.gbServers.Name = "gbServers";
             this.gbServers.Size = new System.Drawing.Size(326, 331);
@@ -85,57 +72,6 @@
             this.cmdConfirm.Text = "Add Server";
             this.cmdConfirm.UseVisualStyleBackColor = true;
             // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(6, 16);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(314, 15);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "abcdefghijklmnopqrstuvwxyz1234567890,./;\'[]{}()";
-            // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(6, 31);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(164, 15);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "abcdefghijklmnopqrstuvwxyz1234567890,./;\'[]{}()";
-            // 
-            // label4
-            // 
-            this.label4.Location = new System.Drawing.Point(176, 31);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 15);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "abcdefghijklmnopqrstuvwxyz1234567890,./;\'[]{}()";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(9, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(90, 49);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(245, 49);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // WindowMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -150,7 +86,6 @@
             this.Name = "WindowMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.WindowMain_Load);
-            this.gbServers.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -161,12 +96,6 @@
         private System.Windows.Forms.GroupBox gbServers;
         private System.Windows.Forms.Button cmdExit;
         private System.Windows.Forms.Button cmdConfirm;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
     }
 }
 
--- A3AT/WindowMain.cs
+++ A3AT/WindowMain.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace A3AT
 {
@@ -33,16 +34,38 @@ namespace A3AT
             string[] testValues2 = new string[3] { "Server 2", "Server IP", "Server port" };
             List<string[]> TestList = new List<string[]>() { testValues1, testValues2 };
 
+            bool success = CreateServerEntry(TestList);
+            Debug.WriteLine("List population result: " + success.ToString());
         }
 
         private bool CreateServerEntry(List<string[]> Servers)
         {
+            int startPosX = 16;
+            int startPosY = 5;
             int[] buttonSize = new int[2] { 75, 23};
             int[] TitleSize = new int[2] { 314, 15 };
             int[] IPSize = new int[2] { 164, 15 };
             int[] PortSize = new int[2] { 43, 15 };
 
-            return false;
+            int i = 1;
+            foreach (string[] entry in Servers)
+            {
+                Debug.WriteLine(Servers.Count.ToString());
+
+                int yPos = (startPosY + TitleSize[1]) * i;
+                Label Title = new Label
+                {
+                    Text = entry[0],
+                    AutoSize = false,
+                    Location = new Point(startPosX, yPos),
+                    Size = new Size(TitleSize[0], TitleSize[1])
+                };
+                gbServers.Controls.Add(Title);
+                Debug.WriteLine("Index " + i + " has been parsed.");
+                i++;
+            }
+
+            return true;
         }
     }
 }
