fad6e7067a9f5a3e6e1e82e879e3d92103cd75b9
nwhitemisc <nwhitemisc@gmail.com>
2018-09-10 14:23:05 -0400

    Initial commit (real one)

---
+++ A3AT/WindowMain.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace A3AT
+{
+    partial class WindowMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbServers = new System.Windows.Forms.GroupBox();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.cmdConfirm = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.gbServers.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arma 3 Administrator Tools";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // gbServers
+            // 
+            this.gbServers.Controls.Add(this.button3);
+            this.gbServers.Controls.Add(this.button2);
+            this.gbServers.Controls.Add(this.button1);
+            this.gbServers.Controls.Add(this.label4);
+            this.gbServers.Controls.Add(this.label3);
+            this.gbServers.Controls.Add(this.label2);
+            this.gbServers.Location = new System.Drawing.Point(12, 78);
+            this.gbServers.Name = "gbServers";
+            this.gbServers.Size = new System.Drawing.Size(326, 331);
+            this.gbServers.TabIndex = 1;
+            this.gbServers.TabStop = false;
+            this.gbServers.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(12, 415);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(75, 23);
+            this.cmdExit.TabIndex = 2;
+            this.cmdExit.Text = "Exit";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // cmdConfirm
+            // 
+            this.cmdConfirm.Location = new System.Drawing.Point(263, 415);
+            this.cmdConfirm.Name = "cmdConfirm";
+            this.cmdConfirm.Size = new System.Drawing.Size(75, 23);
+            this.cmdConfirm.TabIndex = 3;
+            this.cmdConfirm.Text = "Add Server";
+            this.cmdConfirm.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(314, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "abcdefghijklmnopqrstuvwxyz1234567890,./;\'[]{}()";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(6, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "abcdefghijklmnopqrstuvwxyz1234567890,./;\'[]{}()";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(176, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "abcdefghijklmnopqrstuvwxyz1234567890,./;\'[]{}()";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(245, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // WindowMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.cmdConfirm);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.gbServers);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WindowMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.WindowMain_Load);
+            this.gbServers.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbServers;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.Button cmdConfirm;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ A3AT/WindowMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A3AT
+{
+    public partial class WindowMain : Form
+    {
+        public WindowMain()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WindowMain_Load(object sender, EventArgs e)
+        {
+            string[] testValues1 = new string[3] { "Server 1", "Server IP", "Server port" };
+            string[] testValues2 = new string[3] { "Server 2", "Server IP", "Server port" };
+            List<string[]> TestList = new List<string[]>() { testValues1, testValues2 };
+
+        }
+
+        private bool CreateServerEntry(List<string[]> Servers)
+        {
+            int[] buttonSize = new int[2] { 75, 23};
+            int[] TitleSize = new int[2] { 314, 15 };
+            int[] IPSize = new int[2] { 164, 15 };
+            int[] PortSize = new int[2] { 43, 15 };
+
+            return false;
+        }
+    }
+}
